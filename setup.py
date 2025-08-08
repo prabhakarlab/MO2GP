dd6b5a61d91a3f693a5d053a07139c0329bc6d7f
ziyuechen <ellerycn@gmail.com>
2025-08-08 09:12:16 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name="shapealign",
     version="0.1.0",
     description="A package for preprocessing and embedding shape contours using FFT and PCA",
-    author="Your Name",
-    author_email="your.email@example.com",
+    author="Lab of Systems Biology and Data Analytics, GIS, A*STAR, Singapore",
+    author_email="Jagadish_Sankaran@gis.a-star.edu.sg",
     packages=find_packages(),
     install_requires=[
         "numpy",
