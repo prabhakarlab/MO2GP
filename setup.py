4821a8ace3cd26c272d56c98e5fb372b1d92b1d6
Giovani5890 <144999401+Giovani5890@users.noreply.github.com>
2026-06-08 10:46:00 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1.0",
     description="A package for preprocessing and embedding shape contours using FFT and PCA",
     author="Lab of Systems Biology and Data Analytics, GIS, A*STAR, Singapore",
-    author_email="Jagadish_Sankaran@gis.a-star.edu.sg",
+    author_email="Ignasius_Joanito_Irwan@a-star.edu.sg",
     packages=find_packages(),
     install_requires=[
         "numpy",
